0b9360c1f1bcbaa798351fc8a2ccc152db50e776
hieudien14310 <tranhieu14310@gmail.com>
2020-09-06 12:49:35 +0700

    Login with Google

--- src/Config/Passport.js
+++ src/Config/Passport.js
@@ -1,6 +1,7 @@
 const passport = require("passport");
 const localStrategy = require("passport-local").Strategy;
 const facebookStrategy = require('passport-facebook').Strategy
+const googleStrategy = require('passport-google-oauth').OAuth2Strategy
 const User = require("../Models/User");
 
 passport.serializeUser(function (user, done) {
@@ -32,7 +33,7 @@ passport.use(
   )
 );
 
-//Passport Facebook
+//Sign in with Facebook
 passport.use(new facebookStrategy({
   clientID: "833739360498315",
   clientSecret: "1f1fcb09ebdb14afd28727d98eab0b8e",
@@ -60,3 +61,30 @@ async function(accessToken, refreshToken, profile, done) {
   }
 }
 ));
+
+// Sign in with Google
+passport.use(new googleStrategy({
+  clientID: "17328609040-96fno1p6hrg8gvenfv43r5j75tefihlf.apps.googleusercontent.com",
+  clientSecret: "Ov765mJ34-bslDXQYVxie2CX",
+  callbackURL: "http://localhost:3001/login/auth/google/callback",
+  passReqToCallback: true
+},async (req, accessToken, refreshToken, profile, done) => {
+  try {
+    const findUser = await User.findOne({
+      email: profile.emails[0].value,
+      authenType: profile.provider
+    })
+    if(findUser) return done(null, findUser)
+    const newUser = new User({
+      email: profile.emails[0].value,
+      authenType: profile.provider,
+      firstName: profile.name.familyName,
+      lastName: profile.name.givenName,
+      urlImage: profile.photos[0].value
+    })
+    await newUser.save()
+    done(null, newUser)
+  } catch (error) {
+    done(error, false)
+  }
+}))
